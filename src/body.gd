92c92e2ee82a878b7d82a916336fbaf6b803f017
VictorLevrague <victor.levrague@lpsc.in2p3.fr>
2024-08-17 00:21:28 +0200

    Solved str problem

--- src/body.gd
+++ src/body.gd
@@ -1,3 +1,12 @@
-extends Node2D
+extends Resource
 
 class_name Body
+
+var fixed: bool = true
+var x: int = 1
+var y: int = 1
+var size:= Vector2(x, y)
+#var constraint
+
+#func check_constraints():
+#    pass
--- src/screen/game_grid.gd
+++ src/screen/game_grid.gd
@@ -1,7 +1,7 @@
 extends TileMap
 
-var grid_x: int = 4
-var grid_y: int = 4
+var grid_x: int = 3
+var grid_y: int = 2
 var grid_dimension:= Vector2(grid_x, grid_y)
 var game_grid = {}
 
@@ -17,7 +17,7 @@ func fill_game_grid_with_empty_values():
     for x in grid_x:
         for y in grid_y:
             var coordinates:= Vector2(x, y)
-            game_grid[str(coordinates)] = null # TODO: change in null body type
+            game_grid[coordinates] = null # TODO: change in null body type
             set_cell(0, coordinates, 0, Vector2i(0, 0), 0) #layer, coords, source_id_tile, atlas_coord (do not change), alt_tile
 #    game_grid[Vector2(0,0)] = Body.new()
 
@@ -28,7 +28,7 @@ func print_game_grid():
 
 func hover_cells():
     var tile = local_to_map(get_global_mouse_position())
-    if game_grid.has(str(tile)):
+    if game_grid.has(Vector2(tile[0], tile[1])):
         set_cell(1, tile, 3, Vector2i(0, 0), 0)
 
 func erase_hovered_cells():
