4a6e854db06adfe3b5baa159f3c8763991f12f4e
VictorLevrague <victor.levrague@lpsc.in2p3.fr>
2024-08-18 13:01:47 +0200

    Third level with mini sun

---
+++ src/body/resources/mini_sun.gd
@@ -0,0 +1,31 @@
+extends BodyData
+
+#Test simple avec une contrainte de pas de planète à son voisinage
+
+#var constraint_array: Array[Constraint] = []
+
+var constraint1 = Constraint.new()
+var constraint1_description := "Has Sun\n neighbor"
+
+func _init():
+    constraint1.init(false, constraint1_description, has_a_sun_neighbor)
+    constraint_array = [constraint1]
+
+func find_direct_neighbour(game_grid: GridContainer, coordinates: Vector2) ->Array:
+    var neighbours_temp : Array
+    var grid_slots = game_grid.get_children()
+    for grid_slot in grid_slots:
+        var distance = coordinates.distance_to(grid_slot.position/grid_slot.size)
+        if distance <= sqrt(2) + 0.5 and distance > 0:
+            #LE + 0.5 est là pour la marge liées aux pb de normalization. Je pense que je peux l'enlever maintenant, je laisse juste en sécurité.
+            if grid_slot.get_child_count() >0:
+                neighbours_temp.append(grid_slot.get_child(0))
+    return neighbours_temp
+
+func has_a_sun_neighbor(game_grid: GridContainer, coordinates: Vector2) -> bool:
+    var has_sun_around: bool = false
+    var neighbours:Array = find_direct_neighbour(game_grid, coordinates)
+    for neighbour in neighbours:
+        if neighbour.body_data.name == "Sun":
+            has_sun_around = true
+    return has_sun_around
