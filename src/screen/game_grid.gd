cf25f8619f1feddc1f563bc405a153a115f33d88
VictorLevrague <108005152+VictorLevrague@users.noreply.github.com>
2024-08-26 17:32:27 +0200

    Fixed coordinates bug on game-grid
    normalization forgetting h and v separation bug

--- src/screen/game_grid.gd
+++ src/screen/game_grid.gd
@@ -7,10 +7,13 @@ func _ready() -> void:
 
 func check_all_body_constraints(_slot: PanelContainer):
     var are_are_constraints_validated:= true
-    for body_slot in get_children():
-        if body_slot.get_child(0) != null: #ça part du principe que les slot sont initialisés à null. Le test if get_child_count() > 0 serait une sécurité en plus
-            var body:= body_slot.get_child(0)
-            if not body.body_data.check_constraints(body.body_data.constraint_array, self, body_slot.position / body_slot.size):
+    for grid_slot in get_children():
+        if grid_slot.get_child(0) != null: #ça part du principe que les slot sont initialisés à null. Le test if get_child_count() > 0 serait une sécurité en plus
+            var body:= grid_slot.get_child(0)
+            var grid_slot_size_normalized_in_grid = grid_slot.size + Vector2(self["theme_override_constants/h_separation"],
+                                                                     self["theme_override_constants/v_separation"])
+            var grid_slot_position_normalized = grid_slot.position / grid_slot_size_normalized_in_grid
+            if not body.body_data.check_constraints(body.body_data.constraint_array, self, grid_slot_position_normalized):
                 print("failed constraint")
                 are_are_constraints_validated = false
     var inventory_grid = get_parent().get_parent().get_node("InventoryGUI/InventoryGrid")
