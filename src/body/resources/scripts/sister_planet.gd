ab15b8a8ea02f1ac31ade37dc1fb6e4dd578fa86
VictorLevrague <108005152+VictorLevrague@users.noreply.github.com>
2024-08-26 19:28:31 +0200

    Sister init refactor
    The planet condition was not applied to sister planet (difficulty increased now)
    
    I apply it by modifying the original planet constraint to authorize sister planet to be neighbor

--- src/body/resources/scripts/sister_planet.gd
+++ src/body/resources/scripts/sister_planet.gd
@@ -1,31 +1,28 @@
-extends BodyData
+extends Planet
 
-#Test simple avec une contrainte de pas de planète à son voisinage
+var constraint1_bis = Constraint.new()
+var constraint1_bis_description := "No neighboring\n planets\n (except sister)"
 
-#var constraint_array: Array[Constraint] = []
-
-var constraint1 = Constraint.new()
-var constraint1_description := "Has Sister\n Planet neighbor"
+var constraint2 = Constraint.new()
+var constraint2_description := "Has Sister\n Planet neighbor"
 
 func _init():
-    constraint1.init(false, constraint1_description, has_a_sister_neighbor)
-    constraint_array = [constraint1]
+#    planet_init() no need to initialize planet constraint because the behavior has to be changed here to exclude sister planet
+    constraint1_bis.init(false, constraint1_bis_description, has_no_neighbour_planet_except_sister)
+    constraint2.init(false, constraint2_description, has_a_sister_neighbor)
+    constraint_array.append_array([constraint1_bis, constraint2])
 
-func find_direct_neighbour(game_grid: GridContainer, coordinates: Vector2) ->Array:
-    var neighbours_temp : Array
-    var grid_slots = game_grid.get_children()
-    for grid_slot in grid_slots:
-        var distance = coordinates.distance_to(grid_slot.position/grid_slot.size)
-        if distance <= sqrt(2) + 0.5 and distance > 0:
-            #LE + 0.5 est là pour la marge liées aux pb de normalization. Je pense que je peux l'enlever maintenant, je laisse juste en sécurité.
-            if grid_slot.get_child_count() >0:
-                if grid_slot.get_child(0).body_data.name != "Black Hole":
-                    neighbours_temp.append(grid_slot.get_child(0))
-    return neighbours_temp
+func has_no_neighbour_planet_except_sister(game_grid: GridContainer, coordinates_center_body: Vector2) -> bool:
+    var has_planet_around: bool = false
+    var neighbours:Array = game_grid.find_body_in_grid_with_condition(game_grid, coordinates_center_body, func distance_equal_one(x): return x == 1)
+    for neighbour in neighbours:
+        if neighbour.body_data.is_planet and neighbour.body_data.name != "Sister Planet":
+            has_planet_around = true
+    return not has_planet_around
 
-func has_a_sister_neighbor(game_grid: GridContainer, coordinates: Vector2) -> bool:
+func has_a_sister_neighbor(game_grid: GridContainer, coordinates_center_body: Vector2) -> bool:
     var has_sister_around: bool = false
-    var neighbours:Array = find_direct_neighbour(game_grid, coordinates)
+    var neighbours:Array = game_grid.find_body_in_grid_with_condition(game_grid, coordinates_center_body, func distance_equal_one(x): return x == 1)
     for neighbour in neighbours:
         if neighbour.body_data.name == "Sister Planet":
             has_sister_around = true
