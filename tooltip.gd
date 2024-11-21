11ceea75c62d0eb1f6c342faf1615be04f49c19f
VictorLevrague <108005152+VictorLevrague@users.noreply.github.com>
2024-11-21 15:46:00 +0100

    Refactor of tooltip panel
    Maintenant, il n'y a qu'une fenêtre de popup qui est instanciée (automatiquement au début du jeu). Ici aucun risque que la fenêtre se duplique, puisqu'elle n'est générée qu'une seule fois dans le code.
    
    Changement important : je vais refactor la structure des niveaux (commit suivant). Avant, il y avait des grid_slot, qui contenant des GridBody, qui contenait des body_data. Maintenant, il n'y a plus les gridBody, et les body_data sont directement attachées aux slots.
    LEs popup vont donc être affichées quand la souris passe sur le slot et non plus sur le Body (qui n'existe plus directement).
    
    Cette structure simplifiée permet d'éviter bien des bug, et le drag and drop sera toujours implémenté facilement.
    J'avais hésité à faire ça parce que je pensais que ça allait me bloquer pour l'éditeur de niveaux directement dans Godot, mais en fait j'ai une solution (à venir soon)

---
+++ tooltip.gd
@@ -0,0 +1,35 @@
+extends Control
+
+func body_popup(slot_dimensions: Rect2i, body_data: BodyData): #TO DO: type hints
+    if body_data != null:
+        write_body_popup_info(body_data)
+        place_body_popup(slot_dimensions)
+        %BodyPopup.size = Vector2i.ZERO #Reset size of container to miniaml size
+        %BodyPopup.show()
+
+func place_body_popup(slot_dimensions: Rect2i):
+    var mouse_pos = get_viewport().get_mouse_position()
+    var correction: Vector2i
+    var padding = 10
+    if mouse_pos.x <= get_viewport_rect().size.x/2:
+        correction = Vector2i(slot_dimensions.size.x + padding, 0)
+    else:
+        correction = -Vector2i(%BodyPopup.size.x + padding, 0)
+    %BodyPopup.position = slot_dimensions.position + correction
+
+func write_body_popup_info(body_data: BodyData):
+    %Name.text = body_data.name
+    clean_constraint_constainer()
+    for constraint in body_data.constraint_array:
+        var constraint_display = load("res://src/Ui_items/constraint_display.tscn").instantiate()
+        %ConstraintContainer.add_child(constraint_display)
+        constraint_display.get_node("%Description").text = constraint.description
+        constraint_display.get_node("%CheckBox").button_pressed = constraint.is_validated
+
+func clean_constraint_constainer():
+    for children in %ConstraintContainer.get_children():
+        %ConstraintContainer.remove_child(children) #Avoid bug of delayed auto shrinking of containers
+        children.queue_free()
+
+func hide_body_popup():
+    %BodyPopup.hide()
