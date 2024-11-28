52a90118427e324427a85eba14de58692db072f4
VictorLevrague <108005152+VictorLevrague@users.noreply.github.com>
2024-11-28 18:30:23 +0100

    Level 4 refactor
    (previous commit on level 4 was incomplete)

--- src/body/resources/body_scripts/comet.gd
+++ src/body/resources/body_scripts/comet.gd
@@ -15,9 +15,9 @@ func find_body_in_direction(game_grid: GridContainer, coordinates_center_body: V
         var grid_slot_position_normalized = grid_slot.position / grid_slot_size_normalized_in_grid
         var vector_center_body_to_grid_slot = grid_slot_position_normalized - coordinates_center_body
         if vector_center_body_to_grid_slot * direction > Vector2(0,0) and vector_center_body_to_grid_slot.cross(direction) == 0: #Test pour bonne direction (même signe entre vecteur direction et vecteur case ref vers case autre), et bonne ligne (vecteur parallèle au vecteur direction)
-            if grid_slot.get_child_count()>0:
-                if grid_slot.get_child(0).body_data.name != "Black Hole":
-                    bodies_in_front_line.append(grid_slot.get_child(0))
+            if grid_slot.body_data != null:
+                if grid_slot.body_data.name != "Black Hole":
+                    bodies_in_front_line.append(grid_slot)
     return bodies_in_front_line
     
 func has_no_body_in_front_line(game_grid: GridContainer, coordinates_center_body: Vector2) -> bool:
--- src/body/resources/body_scripts/magnet_planet.gd
+++ src/body/resources/body_scripts/magnet_planet.gd
@@ -16,9 +16,9 @@ func find_body_in_front_box(game_grid: GridContainer, coordinates_center_body: V
         var grid_slot_position_normalized = grid_slot.position / grid_slot_size_normalized_in_grid
         var vector_center_body_to_grid_slot = grid_slot_position_normalized - coordinates_center_body
         if vector_center_body_to_grid_slot == direction:
-            if grid_slot.get_child_count()>0:
-                if grid_slot.get_child(0).body_data.name != "Black Hole":
-                    bodies_in_front_line.append(grid_slot.get_child(0))
+            if grid_slot.body_data != null:
+                if grid_slot.body_data.name != "Black Hole":
+                    bodies_in_front_line.append(grid_slot)
     return bodies_in_front_line
 
 func has_body_in_front_box(game_grid: GridContainer, coordinates: Vector2) -> bool:
