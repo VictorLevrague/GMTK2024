6c83b986288949a9c72222d5b96a91bd5c843a97
VictorLevrague <108005152+VictorLevrague@users.noreply.github.com>
2024-12-10 22:04:52 +0100

    Fixed bug where winning level screen on level ui was not the same as winning level screen scene

--- src/buttons/NextLevelButton.gd
+++ src/buttons/NextLevelButton.gd
@@ -1,7 +1,5 @@
 extends Button
 
-@export var next_scene: PackedScene
-
 func _on_button_up() -> void:
     #get_tree().root.get_child(1).get_node("ButtonClick").play()
     Signals.emit_signal("next_level_signal")
